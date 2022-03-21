8a31241af90aad2787a8a78e512ef5b1cda5b3b6
lnatu <dfvutien@gmail.com>
2022-03-21 10:51:15 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.svg">
+    <h2>{{ data.msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Technical Challenge'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+margin: 0;
+font-family: 'Open Sans', sans-serif;
+}
+
+#app {
+margin-top: 60px;
+text-align: center;
+color: #303840;
+}
+
+.logo { max-width: 500px; }
+</style>
